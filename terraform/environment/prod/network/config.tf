ecdc855299e2332fbdc38d7e336d2e8a0824bf47
Shahab nf <shahab.nikfetrat@gmail.com>
2022-04-03 11:00:39 -0400

    Change s3 location to new location for prod

--- terraform/environment/prod/network/config.tf
+++ terraform/environment/prod/network/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "prod-acs730-assignment2-shahab"      // Bucket where to SAVE Terraform State
-    key    = "prod-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    bucket = "tf-prods3-final-project-acs730"      // Bucket where to SAVE Terraform State
+    key    = "prod/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                     // Region where bucket is created
   }
 }
