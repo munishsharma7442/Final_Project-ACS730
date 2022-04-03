13f375f72e0967055283010d1889f415a65488bf
EC2 Default User <ec2-user@ip-172-31-5-222.ec2.internal>
2022-04-03 05:51:43 +0000

    Networking changes  for dev env

---
+++ terraform/environment/dev/network/variable.tf
@@ -0,0 +1,43 @@
+# Default tags
+variable "default_tags" {
+  default = {
+    "Owner" = "Group_9"
+    "App"   = "Web"
+  }
+}
+
+# Prefix to identify resources
+variable "prefix" {
+  type    = string
+  default = "ACS_Project"
+}
+
+# VPC CIDR range
+variable "vpc_cidr" {
+  default     = "10.100.0.0/16"
+  type        = string
+  description = "Dev VPC "
+}
+
+# Provision public subnets in Dev VPC
+variable "public_cidr_blocks" {
+  default     = ["10.100.1.0/24", "10.100.2.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs for Dev"
+}
+
+
+# Provision private subnets in Dev VPC
+variable "private_cidr_blocks" {
+  default     = ["10.100.3.0/24", "10.100.4.0/24"]
+  type        = list(string)
+  description = "Private Subnet CIDRs for Dev"
+}
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "dev"
+  type        = string
+  description = "Deployment Environment"
+}
+
--- terraform/modules/aws_network/variables.tf
+++ terraform/modules/aws_network/variables.tf
@@ -36,17 +36,3 @@ variable "env" {
   type        = string
   description = "Deployment Environment"
 }
-
-# Variable to public - private
-# variable "vpc_type" {
-#   default = "private"
-#   type= string
-#   description="Private subnet"
-# }
-
-# Provision public subnets in custom VPC
-# variable "private_cidr_blocks" {
-#   default     = ["10.20.0.0/24", "10.20.1.0/24"]
-#   type        = list(string)
-#   description = "Public Subnet CIDRs"
-# }
