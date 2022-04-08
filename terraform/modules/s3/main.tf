962ce2c86cfca80a24205ef5da40116e07ce83f3
Shahab nf <shahab.nikfetrat@gmail.com>
2022-04-08 16:01:24 -0400

    Add comment to S3 modules

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,21 +1,21 @@
+
+# Create S3 Bucket
 resource "aws_s3_bucket" "s3" {
   bucket = "tf-${var.env}s3-final-project-acs730"
-  #key    = "${var.env}/network/terraform.tfstate"
-  #bucket = "tf-prods3-final-project-acs730"
-  #region = "us-east-1" 
-
+  
   tags = {
     Name        = "My bucket"
     Environment = "var.env"
   }
 }
 
+# Create ACL for S3
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.s3.id
   acl    = "private"
-
 }
 
+# Limit access to S3
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.s3.id
 
@@ -23,5 +23,4 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_policy     = true
   restrict_public_buckets = true
   ignore_public_acls      = true
-
 }
