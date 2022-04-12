18698d9a0dc468082f3f7c5b6e7d4f2b6fa2d15e
Shahab nf <shahab.nikfetrat@gmail.com>
2022-04-11 22:36:44 -0400

    Add output to LB - Prod

--- terraform/environment/prod/webserver/output.tf
+++ terraform/environment/prod/webserver/output.tf
@@ -3,3 +3,8 @@ output "private_ips" {
   value = aws_instance.my_amazon.*.private_ip
 }
 
+# Print Load Balancer URL
+output "lb_dns_name" {
+  description = "The DNS name of the load balancer."
+  value       = module.alb.lb_dns_name
+}
